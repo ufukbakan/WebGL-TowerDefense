7c8ddff7cec96750232c26fb85ef9855d633d828
Ufuk Bakan <println.ufukbakan@gmail.com>
2022-05-01 19:51:30 +0300

    mouse camera control

--- public/src/initializeCamera.js
+++ public/src/initializeCamera.js
@@ -1,9 +1,14 @@
+const { Euler } = require("three");
+const { Vector3 } = require("three");
 const { PerspectiveCamera } = require("three");
 
 function initializeCamera() {
     const camera = new PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.01, 10);
     const ONE_DEGREE = Math.PI / 180;
-    const CAMERA_STEP = 0.03;
+    const CAMERA_STEP = 0.001;
+    let leftMouseDown = false;
+    let rightMouseDown = false;
+    let nextClickIsDouble = false;
 
     camera.position.z = 1.2;
     camera.position.y = 0.2;
@@ -11,8 +16,69 @@ function initializeCamera() {
 
     var pushedButtons = [];
 
-    window.addEventListener("keydown", keyPush);
-    window.addEventListener("keyup", keyPop);
+    //window.addEventListener("keydown", keyPush);
+    //window.addEventListener("keyup", keyPop);
+    window.addEventListener("wheel", zoom);
+    window.addEventListener("mousedown", toggleDownState);
+    window.addEventListener("mouseup", toggleDownState);
+    window.addEventListener("mousemove", handleCamera);
+    window.addEventListener("contextmenu", waitForDouble);
+
+    /**
+     * 
+     * @param {MouseEvent} e 
+     */
+    function waitForDouble(e){
+        e.preventDefault();
+        if(nextClickIsDouble){
+            camera.rotation.x = -40*ONE_DEGREE;
+            camera.rotation.y = 0;
+            camera.rotation.z = 0;
+        }
+        window.setTimeout(()=>{ nextClickIsDouble = false }, 200);
+        nextClickIsDouble = true;
+    }
+
+    /**
+     * 
+     * @param {MouseEvent} e 
+     */
+    function toggleDownState(e){
+        if(e.button == 0)
+            leftMouseDown = !leftMouseDown;
+        if(e.button == 2)
+            rightMouseDown = !rightMouseDown;
+    }
+
+    /**
+     * @param {MouseEvent} e
+     */
+    function handleCamera(e){
+        if(leftMouseDown){
+            //console.log(e.movementX); // sol eksi sağ artı
+            //console.log(e.movementY); // yukarı eksi aşağı artı
+            camera.position.x -= e.movementX * CAMERA_STEP;
+            camera.position.z -= e.movementY * CAMERA_STEP;
+        }else if(rightMouseDown){
+            camera.rotateX(e.movementY*ONE_DEGREE*0.1);
+            camera.rotateY(e.movementX*ONE_DEGREE*0.1);
+        }
+    }
+
+    /**
+     * 
+     * @param {WheelEvent} e 
+     */
+    function zoom(e){
+        if(e.deltaY > 0){
+            camera.position.y += CAMERA_STEP*50;
+            //camera.zoom *= 0.8;
+        }else if(e.deltaY < 0){
+            camera.position.y += CAMERA_STEP*-50;
+            //camera.zoom *= 1.2;
+        }
+        //camera.updateProjectionMatrix();
+    }
     
     function keyPush(e) {
         const key = e.key;
