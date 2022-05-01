eeb2f5756d01ae8de8efacaa17c393e07515bb79
Ufuk Bakan <println.ufukbakan@gmail.com>
2022-05-01 14:01:41 +0300

    Update main.js

--- public/src/main.js
+++ public/src/main.js
@@ -1,54 +1,28 @@
-const { PerspectiveCamera, Scene, BoxGeometry, MeshNormalMaterial, WebGLRenderer, Mesh } = require("three");
-const setCameraEventListener = require("./camera_control");
-const { GLTFLoader } = require("./GLTFLoader");
+const { Scene, BoxGeometry, MeshNormalMaterial, WebGLRenderer, Mesh } = require("three");
+const initializeCamera = require("./initializeCamera");
 
 window.addEventListener("load", init);
 
-
 function init() {
-	console.log("hello world");
-	const camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-	camera.position.z = 1;
 
-	setCameraEventListener.bind(null, camera)();
+	const camera = initializeCamera();
 
 	const scene = new Scene();
 
 	const geometry = new BoxGeometry(0.2, 0.2, 0.2);
 	const material = new MeshNormalMaterial();
-	
-	const myloader = new GLTFLoader();
-	myloader.load("\\src\\BaseTower.gltf", function(gltf){
-		scene.add(gltf);
-	});
 
 	const mesh = new Mesh(geometry, material);
 	scene.add(mesh);
-
-	// const loader = new GLTFLoader();
-
-	// loader.load('BaseTower.glb', function (gltf) {
-
-	// 	scene.add(gltf.scene);
-
-	// }, undefined, function (error) {
-
-	// 	console.error(error);
-
-	// });
 
 	const renderer = new WebGLRenderer({ antialias: true });
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.setAnimationLoop(animation);
 	document.body.appendChild(renderer.domElement);
 
-	// animation
-
 	function animation(time) {
-
 		//mesh.rotation.x = time / 2000;
 		mesh.rotation.y = time / 1000;
-
 		renderer.render(scene, camera);
 
 	}
