ed7dd38461a847bae8cc7b5e840e88e3ac614924
Ufuk Bakan <println.ufukbakan@gmail.com>
2022-05-01 18:44:20 +0300

    except camera

--- public/src/loadScene.js
+++ public/src/loadScene.js
@@ -1,3 +1,6 @@
+const { TextureLoader } = require("three");
+const { RepeatWrapping } = require("three");
+const { MeshLambertMaterial } = require("three");
 const { PointLight, Mesh, PlaneGeometry, MeshBasicMaterial, Vector3 } = require("three");
 const modelPlacer = require("./modelPlacer");
 const spawnEnemies = require("./spawnEnemies");
@@ -9,8 +12,13 @@ async function loadScene(scene){
 	scene.add(light);
 
 	let objectZ = 1;
+	const textureLoader = new TextureLoader();
 
-    const ground = new Mesh(new PlaneGeometry( 5, 5 ), new MeshBasicMaterial( {color: 0xffff00} ));
+	let groundTexture = textureLoader.load("/src/Assets/grass4.jpg")
+	groundTexture.wrapS = RepeatWrapping;
+	groundTexture.wrapT = RepeatWrapping;
+
+    const ground = new Mesh(new PlaneGeometry( 5, 5 ), new MeshLambertMaterial( { map: groundTexture } ));
 	ground.setRotationFromAxisAngle(new Vector3(1,0,0), -Math.PI/2);
 	scene.add( ground );
 
