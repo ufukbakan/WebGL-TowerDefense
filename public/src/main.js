7aed9cb4b814932a28d3240cb09ead58cd0c3bd2
Ufuk Bakan <println.ufukbakan@gmail.com>
2022-05-04 20:41:22 +0300

    update objects and load scene

--- public/src/main.js
+++ public/src/main.js
@@ -21,20 +21,14 @@ async function init() {
 	renderer.setAnimationLoop(animation);
 	document.body.appendChild(renderer.domElement);
 
-	// const geometry = new BoxGeometry( 1, 1, 1 );
-	// const material = new MeshBasicMaterial( {color: 0x00ff00} );
-	// const cube = new Mesh( geometry, material );
-	// scene.add( cube );
-	// cube.scale.set(0.1,0.1,0.1);
-	// cube.position.set(objects[1].position.x, objects[1].position.y, objects[1].position.z + 0.5);
-
 	function animation(time) {
-
-		// if (counter <= time) {
-
-		// 	counter = time + 100;
-		// }
-
+		scene.traverse(
+			(obj)=>{
+				if(obj.userData.update){
+					obj.userData.update();
+				}
+			}
+		);
 		renderer.render(scene, camera);
 	}
 }
